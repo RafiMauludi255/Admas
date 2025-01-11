2d83690280f58dbea6eaead22995d3b43912db91
RafiMauludi255 <83752075+RafiMauludi255@users.noreply.github.com>
2025-01-11 20:01:42 +0700

    Update main.jsx

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import Index from './Kasir/App'
-// import Index from './Route/'
-// import Index from './Aduan/routes'
-// import Index from './Latihan/Register'
+
 import Index from "./RSCH/routes/index"
 
 createRoot(document.getElementById('root')).render(
