af954a8b74562f294a0e9bd04f7abc3970434b7b
RafiMauludi255 <83752075+RafiMauludi255@users.noreply.github.com>
2025-01-11 20:34:58 +0700

    Update main.jsx, update root file

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Index from "./Admas/routes/index"
+import Index from "./Aduan/routes/index"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
