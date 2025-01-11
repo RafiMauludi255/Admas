2e25c3d4fdb22ef9675661f5dad61ebe7586a227
RafiMauludi255 <83752075+RafiMauludi255@users.noreply.github.com>
2025-01-11 20:13:10 +0700

    Update page.jsx, Tambah deskripsi

--- react-app/src/Aduan/page.jsx
+++ react-app/src/Aduan/page.jsx
@@ -1,3 +1,5 @@
+// Halaman Utama
+
 import Navbar from "./component/navbar";
 import Body from "./component/body";
 import "./style/master.css"
@@ -5,7 +7,10 @@ import "./style/master.css"
 export default function Page() {
   return (
     <div className="master">
+{/*  Component Navbar  */}
       <Navbar />
+
+{/*  Compoenent Body  */}
       <Body />
     </div>
   )
